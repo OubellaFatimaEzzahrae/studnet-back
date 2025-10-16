c6d590fb5a15d517fabd9b8b4eb4c6724bdb60b7
OubellaFatimaEzzahrae <fatimaezzahraeoubella@gmail.com>
2025-10-16 17:29:00 +0100

    feat: 4th commit

--- src/main/java/com/studnet/controller/CourseController.java
+++ src/main/java/com/studnet/controller/CourseController.java
@@ -9,6 +9,11 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
+//Solution de dépannage
+/**
+ * @todo Trouver les différents solutions pour éviter ce genre de prblems */
+@CrossOrigin(origins = "http://localhost:4200")  // autorise Angular
 @RestController
 @RequestMapping("/api/courses")
 public class CourseController {
@@ -34,6 +39,19 @@ public class CourseController {
         Major savedMajor = courseService.saveCourse(major);
         return new ResponseEntity<>(savedMajor, HttpStatus.CREATED);
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Major> updateMajor(@PathVariable int id, @RequestBody Major major) {
+        return courseService.getMajorById(id)
+                .map(existingMajor -> {
+                    existingMajor.setCode(major.getCode());
+                    existingMajor.setDescription(major.getDescription());
+                    Major updated = courseService.saveCourse(existingMajor);
+                    return ResponseEntity.ok(updated);
+                })
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteMajor(@PathVariable int id) {
